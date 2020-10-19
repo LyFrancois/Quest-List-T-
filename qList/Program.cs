2b669a562778faf1542ae76a48163068f2335b94
Chahine Ly <lyungkari@hotmail.fr>
2020-10-19 12:59:06 +0200

    Quest List<v>

---
+++ qList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace qList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            List<char> CharCollection = new List<char>();
+            CharCollection.Add('R');
+            CharCollection.Add('D');
+            CharCollection.Add('V');
+
+            CharCollection.RemoveAt(0);
+            CharCollection.Remove('V');
+            CharCollection[1] = 'Y';
+
+            Console.WriteLine(CharCollection[1]);
+
+
+
+
+        }
+    }
+}
